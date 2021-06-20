c2fb526cf38f022b9c47906044aa86372670676b
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-06-20 18:36:20 +0300

    add overlay effect

--- js/scripts.js
+++ js/scripts.js
@@ -31,10 +31,60 @@ $(document).ready(function() {
 $(".image1").mouseover(function(){
     $("#one").show();
 });
-
 $(".image1").mouseout(function(){
     $("#one").hide();
 });
+
+$(".image2").mouseover(function(){
+    $("#two").show();
+});
+$(".image2").mouseout(function(){
+    $("#two").hide();
+});
+
+$(".image3").mouseover(function(){
+    $("#three").show();
+});
+$(".image3").mouseout(function(){
+    $("#three").hide();
+});
+
+$(".image4").mouseover(function(){
+    $("#four").show();
+});
+$(".image4").mouseout(function(){
+    $("#four").hide();
+});
+
+
+$(".image5").mouseover(function(){
+    $("#five").show();
+});
+$(".image5").mouseout(function(){
+    $("#five").hide();
+});
+
+$(".image6").mouseover(function(){
+    $("#six").show();
+});
+$(".image6").mouseout(function(){
+    $("#six").hide();
+});
+
+$(".image7").mouseover(function(){
+    $("#seven").show();
+});
+$(".image7").mouseout(function(){
+    $("#seven").hide();
+});
+
+$(".image8").mouseover(function(){
+    $("#eight").show();
+});
+$(".image8").mouseout(function(){
+    $("#eight").hide();
+});
+
 
 });
 
